6cd7e10b854973cb646183578b4b7158c9bf153a
rzsptr <rzsptr02@gmail.com>
2026-07-15 17:11:45 +0200

    Update importer.py

--- ├── app/importer.py
+++ ├── app/importer.py
@@ -1,1 +1,10 @@
+from openpyxl import load_workbook
 
+
+class ExcelImporter:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def load(self):
+        workbook = load_workbook(self.filename, data_only=True)
+        return workbook
